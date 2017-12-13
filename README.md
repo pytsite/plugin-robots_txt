7864463b8d01ede844cf6a76da2b1f0c700eae45
Alexander Shepetko <a@shepetko.com>
2017-12-13 18:08:03 +0200

    Release 1.1

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
-PytSite robots.txt Plugin
-==========================
+# PytSite robots.txt Plugin
 
-Changelog
----------
+
+## Changelog
+
+
+### 1.1 (2017-12-13)
+
+Support for PytSite-7.0.
 
 
 ### 1.0 (2017-11-25)
--- __init__.py
+++ __init__.py
@@ -1,19 +1,18 @@
-"""Pytsite robots.txt support.
+"""Pytsite robots.txt Plugin
 """
-
-# Public API
-from ._api import disallow, sitemap
-
 __author__ = 'Alexander Shepetko'
 __email__ = 'a@shepetko.com'
 __license__ = 'MIT'
 
+from pytsite import plugman as _plugman
 
-def _init():
+if _plugman.is_installed(__name__):
+    # Public API
+    from ._api import disallow, sitemap
+
+
+def plugin_load():
     from pytsite import cron
     from . import _eh
 
     cron.daily(_eh.cron_daily)
-
-
-_init()
